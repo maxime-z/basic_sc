65e2144cbd4b683af095e747f59cf6de14cefd23
maxime-z <maxime.zh@gmail.com>
2019-06-12 09:34:48 +0200

    complete Implicit Euler Solution and compare it with Explicit Euler and Exact Solution.

--- ODE/stiff_problem.py
+++ ODE/stiff_problem.py
@@ -11,6 +11,7 @@ from sympy.solvers.ode import dsolve
 
 
 def analytical_solution():
+    """Resolve exact solution using Sympy"""
     t, a = symbols('t a')
     x = Function('x')(t)
     dx = -a * x
@@ -25,10 +26,7 @@ def analytical_solution():
 ### Secondly, let's try to solve the problem using Euler explicit
 
 def explicit_euler_solution(dx, x0, dt, n_step):
-    # Initial condition and first order derivative
-    x0 = 1
-    dx = lambda x: -a * x
-
+    """Numerical solution found through Euler explicit method"""
     t = np.arange(0, n_step) * dt
     x_num = np.zeros(n_step)
     x_num[0] = x0
@@ -38,14 +36,30 @@ def explicit_euler_solution(dx, x0, dt, n_step):
     return t, x_num
 
 
-def implicit_euler_solution(a, dt, n_step):
-    pass
+def implicit_euler_solution(a, x0, dt, n_step):
+    """Numerical solution found through Euler implicit method"""
+    t = np.arange(0, n_step)*dt
+    x_num = np.zeros(n_step)
+    x_num[0] = x0
+    for i in range(1, n_step):
+        x_num[i] = 1/(1+a*dt)*x_num[i-1]
+
+    return t, x_num
 
 
 if __name__ == '__main__':
+    # Initial condition and first order derivative
+    x0 = 1
+    dx = lambda x: -a * x
     a = 10
 
-    dt = 0.12
-    n_step = 10
-    plt.plot(*explicit_euler_solution(a, dt, n_step))
+    dt = 0.15
+    n_step = 20
+    t, x_explicit = explicit_euler_solution(dx, x0, dt, n_step)
+    t, x_implicit = implicit_euler_solution(a, x0, dt, n_step)
+    x_exact = np.exp(-a * t)
+    plt.plot(t, x_explicit, 'b')
+    plt.plot(t, x_implicit, 'k')
+    plt.plot(t, x_exact, 'r')
+
     plt.show()
